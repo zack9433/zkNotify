e16aebc150d14544b0ac9080d65f9e0860e96064
zackyang <zack9433@gmail.com>
2014-03-11 18:11:17 +0800

    Add notify status

--- angular-zk-notify.js
+++ angular-zk-notify.js
@@ -7,29 +7,55 @@ var zkNotifyMgr = function(zkNotifySrv) {
   this.getNotifyMsg = function() {
     return zkNotifySrv.getNotifyMsg();
   };
+  this.getNotifyMsgStatus = function() {
+    return zkNotifySrv.getNotifyMsgStatus();
+  };
 };
 zkNotifyMgr.$inject = ['zkNotifySrv'];
 
 angular.module('zkNotify', [])
-  .service('zkNotifySrv', [function zkNotifySrv() {
+  .service('zkNotifySrv', ['$rootScope', '$timeout', function zkNotifySrv($rootScope, $timeout) {
+    var timer, timeout = 6000;
+
     this.message = null;
+    this.isToggleNotifyMsg = false;
 
     this.setNotifyMsg = function(msg) {
-      this.message = msg;
+      var self = this;
+      if (timer) {
+        $timeout.cancel(timer);
+      }
+      self.message = msg;
+      self.toggleNotifyMsg();
+      timer = $timeout(function() {
+        self.toggleNotifyMsg();
+      }, timeout);
     };
 
     this.getNotifyMsg = function() {
       return this.message;
     };
+
+    this.toggleNotifyMsg = function() {
+      this.isToggleNotifyMsg = !this.isToggleNotifyMsg;
+    };
+
+    this.getNotifyMsgStatus = function() {
+      return this.isToggleNotifyMsg;
+    };
+
+    $rootScope.$on('$destory', function() {
+      $timeout.cancel(timer);
+    });
   }])
   .directive('zkNotify', ['$controller', function ($controller) {
     return {
       templateUrl: 'angular-zk-notify.html',
       restrict: 'EA',
+      replace: true,
       scope: {},
       link: function postLink(scope, element, attrs) {
-        var notifyMgr = $controller(zkNotifyMgr);
-        scope.message = notifyMgr.getNotifyMsg();
+        scope.notifyMgr = $controller(zkNotifyMgr);
       }
     };
   }]);
