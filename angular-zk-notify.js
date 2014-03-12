2bb36d610f5fc84769171a7e94cc2c5b449c55b8
zackyang <zack9433@gmail.com>
2014-03-12 11:14:30 +0800

    Modify zk notify logic

--- angular-zk-notify.js
+++ angular-zk-notify.js
@@ -1,61 +1,51 @@
 'use strict';
 
-var zkNotifyMgr = function(zkNotifySrv) {
-  this.setNotifyMsg = function(msg) {
-    zkNotifySrv.setNotifyMsg(msg);
-  };
-  this.getNotifyMsg = function() {
-    return zkNotifySrv.getNotifyMsg();
-  };
-  this.getNotifyMsgStatus = function() {
-    return zkNotifySrv.getNotifyMsgStatus();
-  };
-};
-zkNotifyMgr.$inject = ['zkNotifySrv'];
-
 angular.module('zkNotify', [])
-  .service('zkNotifySrv', ['$rootScope', '$timeout', function zkNotifySrv($rootScope, $timeout) {
-    var timer, timeout = 6000;
-
+  .service('zkNotifySrv', [function zkNotifySrv() {
     this.message = null;
-    this.isToggleNotifyMsg = false;
 
     this.setNotifyMsg = function(msg) {
-      var self = this;
-      if (timer) {
-        $timeout.cancel(timer);
-      }
-      self.message = msg;
-      self.toggleNotifyMsg();
-      timer = $timeout(function() {
-        self.toggleNotifyMsg();
-      }, timeout);
+      this.message = msg;
     };
 
     this.getNotifyMsg = function() {
       return this.message;
     };
 
-    this.toggleNotifyMsg = function() {
-      this.isToggleNotifyMsg = !this.isToggleNotifyMsg;
-    };
-
-    this.getNotifyMsgStatus = function() {
-      return this.isToggleNotifyMsg;
-    };
-
-    $rootScope.$on('$destory', function() {
-      $timeout.cancel(timer);
-    });
   }])
-  .directive('zkNotify', ['$controller', function ($controller) {
+  .directive('zkNotify', ['$timeout', '$rootScope', 'zkNotifySrv', function ($timeout, $rootScope, zkNotifySrv) {
     return {
       templateUrl: 'angular-zk-notify.html',
       restrict: 'EA',
       replace: true,
       scope: {},
       link: function postLink(scope, element, attrs) {
-        scope.notifyMgr = $controller(zkNotifyMgr);
+        var timeout = 6000;
+        if (attrs.timeout) {
+          timeout = scope.$eval(attrs.timeout);
+        }
+
+        scope.isNotify = false;
+        attrs.$observe('message', function(value) {
+          if (!value) {
+            scope.message = zkNotifySrv.getNotifyMsg();
+          } else {
+            scope.message = value;
+            scope.isNotify = true;
+            $timeout(function() {
+              scope.isNotify = false;
+            }, timeout);
+          }
+        });
+
+        scope.$watch(function() {
+          return zkNotifySrv.getNotifyMsg();
+        }, function(newVal) {
+          scope.isNotify = true;
+          $timeout(function() {
+            scope.isNotify = false;
+          }, timeout);
+        });
       }
     };
   }]);
