d98e0dcee782ea23f7b0d79978471ef834344ddd
zackyang <zack9433@gmail.com>
2014-06-19 11:54:21 +0800

    Modify

--- angular-zk-notify.js
+++ angular-zk-notify.js
@@ -1,78 +1,78 @@
-'use strict';
+;(function() { 'use strict';
+  angular.module('zkNotify', [])
+    .constant('NOTIFY_TIMEOUT', 3000)
+    .service('zkNotifySrv', [function zkNotifySrv() {
+      this.message = null;
 
-angular.module('zkNotify', [])
-  .constant('NOTIFY_TIMEOUT', 6000)
-  .service('zkNotifySrv', [function zkNotifySrv() {
-    this.message = null;
+      this.setNotifyMsg = function(msg) {
+        this.message = msg;
+      };
 
-    this.setNotifyMsg = function(msg) {
-      this.message = msg;
-    };
+      this.getNotifyMsg = function() {
+        return this.message;
+      };
 
-    this.getNotifyMsg = function() {
-      return this.message;
-    };
+    }])
+    .directive('zkNotify',
+      ['$rootScope', '$timeout', 'zkNotifySrv', 'NOTIFY_TIMEOUT',
+      function ($rootScope, $timeout, zkNotifySrv, NOTIFY_TIMEOUT) {
+      return {
+        templateUrl: 'angular-zk-notify.html',
+        restrict: 'EA',
+        replace: true,
+        scope: {},
+        link: function postLink(scope, element, attrs) {
+          var timeout = NOTIFY_TIMEOUT, timer;
 
-  }])
-  .directive('zkNotify',
-    ['$rootScope', '$timeout', 'zkNotifySrv', 'NOTIFY_TIMEOUT',
-    function ($rootScope, $timeout, zkNotifySrv, NOTIFY_TIMEOUT) {
-    return {
-      templateUrl: 'angular-zk-notify.html',
-      restrict: 'EA',
-      replace: true,
-      scope: {},
-      link: function postLink(scope, element, attrs) {
-        var timeout = NOTIFY_TIMEOUT, timer;
-
-        if (attrs.timeout) {
-          timeout = scope.$eval(attrs.timeout);
-        }
-
-        element.css('display', 'none');
-        attrs.$observe('message', function(value) {
-          if (!value) {
-            scope.notify = zkNotifySrv;
-          } else {
-            scope.notify = {
-              message: value
-            };
-            $timeout(function() {
-              element.css('display', 'block');
-              scope.notify.message = '';
-            }, timeout);
+          if (attrs.timeout) {
+            timeout = scope.$eval(attrs.timeout);
           }
-        });
 
-        scope.closeNotify = function() {
-          if (timer) {
-            $timeout.cancel(timer);
-          }
           element.css('display', 'none');
-          scope.notify.message = '';
-        };
+          attrs.$observe('message', function(value) {
+            if (!value) {
+              scope.notify = zkNotifySrv;
+            } else {
+              scope.notify = {
+                message: value
+              };
+              $timeout(function() {
+                element.css('display', 'block');
+                scope.notify.message = '';
+              }, timeout);
+            }
+          });
 
-        scope.$watch('notify.message', function(newVal) {
-          if (!newVal) {
-            return;
-          }
+          scope.closeNotify = function() {
+            if (timer) {
+              $timeout.cancel(timer);
+            }
+            element.css('display', 'none');
+            scope.notify.message = '';
+          };
 
-          if (timer) {
-            $timeout.cancel(timer);
-          }
+          scope.$watch('notify.message', function(newVal) {
+            if (!newVal) {
+              return;
+            }
 
-          if (!attrs.message) {
-            element.css('display', 'block');
-            timer = $timeout(function() {
-              element.css('display', 'none');
-              zkNotifySrv.setNotifyMsg('');
-            }, timeout);
-          }
-        });
+            if (timer) {
+              $timeout.cancel(timer);
+            }
 
-        $rootScope.$on('$locationChangeSuccess', function() {
-          scope.closeNotify();
-        });
-      }
-    };
-  }]);
+            if (!attrs.message) {
+              element.css('display', 'block');
+              timer = $timeout(function() {
+                element.css('display', 'none');
+                zkNotifySrv.setNotifyMsg('');
+              }, timeout);
+            }
+          });
+
+          $rootScope.$on('$locationChangeSuccess', function() {
+            scope.closeNotify();
+          });
+        }
+      };
+    }]);
+}());
